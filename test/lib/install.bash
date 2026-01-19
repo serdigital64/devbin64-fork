ab0e0ff2efc4a00a3b33294b7423d6365b954212
SerDigital64 (Max) <serdigital64@gmail.com>
2026-01-19 14:56:06 +0100

    real: promote 12.3.0 (#80)

--- test/lib/install.bash
+++ test/lib/install.bash
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
-# Install DevBin64 in test environment
+[[ -n "$DEV_CICD_DEBUG_BASH" ]] && set -x
 
+if [[ -d "$TEST_INSTALL_TARGET" ]]; then
+  rm -Rf "$TEST_INSTALL_TARGET"
+fi
+
+if [[ -d "$TEST_TARGET" ]]; then
+  rm -Rf "$TEST_TARGET"
+fi
+
+# Install DevBin64 in test environment
 mkdir -p \
   "$TEST_INSTALL_TARGET" \
   "${TEST_INSTALL_TARGET}/bin" \
   "$TEST_TARGET" &&
   cp -r "${TEST_SOURCE_LIB}/lib" "$TEST_INSTALL_TARGET" &&
-  cp "${TEST_SOURCE_LIB}/lib/devbin64/bin/dev-bootstrap" "${TEST_INSTALL_TARGET}/bin" &&
-  cd "$TEST_TARGET" &&
-  "${TEST_INSTALL_TARGET}/bin/dev-bootstrap" >/dev/null
+  cp "${TEST_SOURCE_LIB}/lib/devbin64/bin/dev-bootstrap" "${TEST_INSTALL_TARGET}/bin" ||
+  exit $?
+
+[[ -n "$DEV_CICD_DEBUG_BASH" ]] && set +x
+
+cd "$TEST_TARGET" &&
+  "${TEST_INSTALL_TARGET}/bin/dev-bootstrap"
